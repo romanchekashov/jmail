f151417f14177dc6cef1ec1ba6bba56f6f66580b
rchekashov <romanrich89@gmail.com>
2023-10-03 21:43:39 +0200

    fix open DMG app on Mac

--- src/main/java/ovh/look/jmail/utils/AssetUtils.java
+++ src/main/java/ovh/look/jmail/utils/AssetUtils.java
@@ -7,6 +7,9 @@ import java.awt.image.BufferedImage;
 import java.io.*;
 import java.net.URISyntaxException;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class AssetUtils {
 
@@ -33,5 +36,19 @@ public class AssetUtils {
         InputStream is = JMail.class.getResourceAsStream(path);
         InputStreamReader streamReader = new InputStreamReader(is, StandardCharsets.UTF_8);
         return new BufferedReader(streamReader);
+    }
+
+    public static void createFile(){
+        try {
+//            Path path = Paths.get(System.getProperty("user.home") + "/newfile.txt");
+//            Path path = Paths.get("/Users/romanchekashov/newfile.txt");
+            Path path = Paths.get("/Applications/JMail.app/Contents/newfile.txt");
+//            Path path = Paths.get(getCurrentPath() + "/newfile.txt");
+            // Create the file
+            Files.createFile(path);
+            System.out.println("File created: " + path.toAbsolutePath());
+        } catch (IOException e) {
+            System.err.println("Error creating the file: " + e.getMessage());
+        }
     }
 }
